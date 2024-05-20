5ddf0b71effafc375b1beabd792eef3d54daccef
Doris <72460384+dorisbrkic@users.noreply.github.com>
2024-05-20 15:23:40 +0200

    Update making_bed_sex_phenotype.sh

--- PLINK/GWAS/making_bed_sex_phenotype.sh
+++ PLINK/GWAS/making_bed_sex_phenotype.sh
@@ -11,18 +11,17 @@ cd $PBS_O_WORKDIR
 
 
 # ----------------Commands------------------- #
-#recalibrated data 77
 
-plink --vcf /common/WORK/dbrkic/Projekti/COVID/WES_Analysis/GWAS/recalibrated/july/snps77_ann_all.vcf.gz --recode --keep keep_samples.txt --out snps77_ann
+#keep_samples keeps only samples with high heterozygocity
+plink --vcf rmdup_renam_sort.vcf.gz --make-bed --keep keep_samples.txt --out chr122xy_mt189
 
 #add sex
-plink --file snps77_ann --update-sex /common/WORK/dbrkic/Projekti/COVID/WES_Analysis/GWAS/sex_file_3.list --make-bed --out snps3_ann_sex
-
-#fix sex
-plink --bfile snps3_ann_sex --impute-sex 0.964 0.969 --make-bed --out snps3_ann_sex_imp
+plink --bfile chr122xy_mt189 --update-sex /common1/WORK/dbrkic/Projekti/COVID/WES_Analysis/GWAS/sex_file_9.list --make-bed --out chr122xy_mt189_sex
 
 #check sex
-plink --bfile snps3_ann_sex_imp --check-sex --out snps3_ann_imp
+plink --bfile chr122xy_mt189_sex --check-sex --out chr122xy_mt189_sex
 
-#add phenotype
-plink --bfile snps3_ann_sex --pheno /common/WORK/dbrkic/Projekti/COVID/WES_Analysis/GWAS/pheno_march.list --make-bed --out snps3_ann_sex_pheno
+#fix sex
+plink --bfile chr122xy_mt189_sex --impute-sex 0.965 0.969 --make-bed --out chr122xy_mt189_sex_imp
+
+
